1384070e26e0e2fdaefe520623b77da7b4f64dac
gab246 <119339137+gab246@users.noreply.github.com>
2023-07-14 11:06:42 +0200

    added firebaseConfig

--- App.js
+++ App.js
@@ -1,15 +1,34 @@
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
-//import screens
-import Start from './components/Start';
-import Chat from './components/Chat';
 //react navigation
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 //create navigator 
 const Stack = createNativeStackNavigator();
 
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+//import screens
+import Start from './components/Start';
+import Chat from './components/Chat';
+
 const App = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyBC4KuSMqboZsx_spI_192LoVlMVzif0JU",
+    authDomain: "chat-app-28a30.firebaseapp.com",
+    projectId: "chat-app-28a30",
+    storageBucket: "chat-app-28a30.appspot.com",
+    messagingSenderId: "665783359835",
+    appId: "1:665783359835:web:7843c793e3240002b5a27e"
+  };
+ 
+
+  //initialises Firebase 
+  const app = initializeApp(firebaseConfig);
+  //initialises Cloud Firestire, gets reference to service 
+  const db = getFirestore(app);
+
   return (
     <NavigationContainer style={styles.container}>
       <Stack.Navigator
@@ -23,6 +42,7 @@ const App = () => {
         name='Chat'
         component={Chat}
         />
+        {props => <Chat db={db} {...props} />}
       </Stack.Navigator>
     </NavigationContainer>
   );
