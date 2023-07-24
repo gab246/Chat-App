6e2fe04ee2939f51d0541c2087032d8b06d46b57
gab246 <119339137+gab246@users.noreply.github.com>
2023-07-24 16:18:14 +0200

    updated firestore

--- App.js
+++ App.js
@@ -3,34 +3,33 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Start from "./components/Start"
-import Chat from "./components/chat"
 
-//create navigator 
+//create navigator
 const Stack = createNativeStackNavigator();
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import Chat from './components/Chat';
 
 
 
 const App = () => {
   const firebaseConfig = {
-    apiKey: "AIzaSyArTIqT_HkdoHVHpBfm4865qHlamNg2RHs",
-    authDomain: "chat-app2-a9104.firebaseapp.com",
-    projectId: "chat-app2-a9104",
-    storageBucket: "chat-app2-a9104.appspot.com",
-    messagingSenderId: "904969986062",
-    appId: "1:904969986062:web:f47a066cd38851cac5dc5f"
+    apiKey: "AIzaSyBym5So4U03Nm_tcFomlf6Vx_Q2O3wkLro",
+    authDomain: "chat-app-b84bb.firebaseapp.com",
+    projectId: "chat-app-b84bb",
+    storageBucket: "chat-app-b84bb.appspot.com",
+    messagingSenderId: "888136623373",
+    appId: "1:888136623373:web:e36711dcf822b1e9107c44"
   };
 
 
-  //initialises Firebase 
+  //initialises Firebase
   const app = initializeApp(firebaseConfig);
-  //initialises Cloud Firestire, gets reference to service 
+  //initialises Cloud Firestire, gets reference to service
   const db = getFirestore(app);
-  
+
 
   return (
     <NavigationContainer style={styles.container}>
@@ -38,14 +37,14 @@ const App = () => {
       initialRouteName='Start' /* first screen to load*/
       >
         <Stack.Screen
-        name='Start' 
+        name='Start'
         component={Start} /* component to display on screen */
         />
         <Stack.Screen
         name='Chat'
-        component={Chat}
-        />
-        {props => <Chat db={db} {...props} />} 
+        >
+        {props => <Chat db={db} {...props} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
